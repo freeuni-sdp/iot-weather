89a6ff3448654a71b29e68e997c9b84cb339a044
meripepanashvili <mpepa13@freeuni.edu.ge>
2016-06-10 02:11:57 +0400

    pom dependencies

--- test/test.java
+++ test/test.java
@@ -1,5 +1,30 @@
 /**
  * Created by Meko on 10/06/2016.
  */
-public class test {
+import ge.edu.freeuni.sdp.weather.core.PingService;
+import java.util.*;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.*;
+import static org.junit.Assert.*;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import com.microsoft.azure.storage.StorageException;
+import org.junit.Test;
+public class test extends JerseyTest{
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(PingService.class);
+    }
+
+    @Test
+    public void ping_test(){
+        Response actual = target("ping").request().get();
+        assertEquals(
+                Response.Status.OK.getStatusCode(),
+                actual.getStatus());
+    }
+
 }
