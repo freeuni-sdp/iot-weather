a2f34ae8f4855bc0961d48c51889ddceb6e921ae
meripepanashvili <mpepa13@freeuni.edu.ge>
2016-07-11 03:26:12 +0400

    Temperature Test Finish

--- test/TemperatureTest.java
+++ test/TemperatureTest.java
@@ -4,10 +4,12 @@ import ge.edu.freeuni.sdp.iot.service.weather.service.WeatherService;
 import org.easymock.Mock;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
-import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.Application;
+
+import static ge.edu.freeuni.sdp.iot.service.weather.service.Constants.HOUSE_SERVICE_URL;
+import static ge.edu.freeuni.sdp.iot.service.weather.service.Constants.WEATHER_API_URL;
 
 /**
  * Created by Meko on 10/07/2016.
@@ -25,19 +27,19 @@ public class TemperatureTest extends JerseyTest {
         return new ResourceConfig(WeatherService.class);
     }
 
-    @Before
-    public void setup(){
-        houseID = "a1bc00fb-4724-476a-a553-d65d2c1bac0e";
-    }
-
-
     @Test
     public void test_temperature(){
-        String json = WeatherService.getJsonText("http://iot-weather.herokuapp.com/webapi/houses/"+ houseID +"/temperature");
-        System.out.print(json);
-
-        JsonPath.read(json, "$.temp_f");
-        assert(true);
+        try {
+            String json = WeatherService.getJsonText(HOUSE_SERVICE_URL);
+            String houseID = JsonPath.read(json, "$[0].RowKey._");
+            json = WeatherService.getJsonText(WEATHER_API_URL + houseID + "/temperature");
+            int f = JsonPath.read(json, "$.fahrenheit");
+            int c = JsonPath.read(json, "$.celsius");
+            assert(c > -100 && c < 65 && f > -130 && f < 140);
+        }
+        catch (Exception ex) {
+            assert(false);
+        }
     }
 
 }
