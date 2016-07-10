bd93ddf1e4c2b57458e934e9f44a5567b869ccd4
meripepanashvili <mpepa13@freeuni.edu.ge>
2016-07-10 21:08:23 +0400

    Constants change

--- src/main/java/ge/edu/freeuni/sdp/iot/service/weather/service/Constants.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/weather/service/Constants.java
@@ -5,7 +5,7 @@ package ge.edu.freeuni.sdp.iot.service.weather.service;
  */
 public final class Constants {
     public static final String HOUSE_SERVICE_URL =
-            "http://private-3c24-iothouseregistry.apiary-mock.com/houses/";
+            "http://iot-house-registry.herokuapp.com/houses/";
     public static final String WEATHER_CONDITIONS_URL =
             "http://api.wunderground.com/api/147c55ed4df9f4cf/conditions/q/";
     public static final String HOURLY_FOREST_URL =
